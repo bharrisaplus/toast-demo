57b011910987f3bcc87b8772caeb8dbebd8699ca
Barry Harris <bharrisaplus@gmail.com>
2026-03-17 22:09:18 +0100

    Add version 2 and update readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Toast Demo
 An example of notifications using the [popover API](https://developer.mozilla.org/en-US/docs/Web/API/Popover_API)
 
-[Live example](https://codepen.io/editor/Shadeslayer345/pen/019cf97c-4818-7f29-aafb-3f7390537130) on [codepen](https://codepen.io)
+Live example on [codepen](https://codepen.io)
+
+## Version 1
+Uses the individual notifications as popover elements
+[Demo 1](https://codepen.io/editor/Shadeslayer345/pen/019cf97c-4818-7f29-aafb-3f7390537130)
+
+
+## Version 2
+Uses the notification area as a single popover element
+[Demo 2](https://codepen.io/editor/Shadeslayer345/pen/019cf97c-4818-7f29-aafb-3f7390537130) on [codepen](https://codepen.io)
---
+++ attempt_2/main.mjs
@@ -0,0 +1,74 @@
+const makePageApp = () => {
+  let
+    notifications = [],
+    uiCount = 1;
+  const $base = document.querySelector('template#toast-container');
+
+  const add_notification = () => {
+    const
+      _$container = document.importNode($base.content, true),
+      _$notification = _$container.querySelector('.toast'),
+      _notiAbortController = new AbortController();
+
+    let notiMsg = _$notification.querySelector('p').textContent;
+
+    _$notification.querySelector('p').textContent = `${notiMsg}, Numbah ${uiCount++}`;
+    
+    _$notification.querySelector('button.close').addEventListener('click', (_clickEvt) => {
+      let
+        toRemove,
+        _gcClose;
+
+      if (_clickEvt.target.parentNode !== _$notification) { return; }
+
+      toRemove = notifications.map((notiItm) => notiItm.$elm ).indexOf(_$notification);
+
+      if (notifications.length == 1) {
+        document.querySelector('#toaster').hidePopover();
+      }
+
+      if (toRemove == -1) {
+        _$notification.style.setProperty('opacity', '0');
+        _$notification.remove();
+        return;
+      }
+
+      _gcClose = notifications[toRemove];
+      _gcClose.cancel.abort();
+      _gcClose.$elm.style.setProperty('opacity', '0');
+      notifications = notifications.toSpliced(toRemove, 1);
+      _gcClose.$elm.remove();
+    }, {signal: _notiAbortController.signal});
+
+    document.querySelector('#toaster').appendChild(_$notification);
+    document.querySelector('#toaster').showPopover();
+
+    if (notifications.length >= 3) {
+      const _gcBye = notifications.shift();
+
+      _gcBye.cancel.abort();
+      _gcBye.$elm.style.setProperty('opacity', '0');
+      _gcBye.$elm.remove();
+    }
+
+    notifications.push({$elm: _$notification, cancel: _notiAbortController});
+    
+  };
+
+  return Object.freeze({
+    boot: () => { console.log("app started"); },
+    newToast: add_notification
+  });
+};
+
+let singlePageApp;
+
+const getSinglePageApp = () => {
+  if (!singlePageApp) {
+    singlePageApp = makePageApp();
+  }
+
+  return singlePageApp;
+};
+
+export default makePageApp;
