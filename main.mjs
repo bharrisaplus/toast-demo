5dc4b5d1b2d87565dd43f9c2003deeee3b250258
Barry Harris <bharrisaplus@gmail.com>
2026-03-17 22:02:15 +0100

    Update source files

--- main.mjs
+++ main.mjs
@@ -1,32 +1,33 @@
 const makePageApp = () => {
   let
     notifications = [],
-    sweeps = [];
+    uiCount = 1;
   const $base = document.querySelector('template#toast-container');
 
   const add_notification = () => {
     const
       _$container = document.importNode($base.content, true),
-      _$notification = _$container.querySelector('.toast'),
-      _notiCancel = new AbortController(),
-      _notiAbortSignal = _notiCancel.signal;
+      _$notification = _$container.querySelector('.toast');
+
+    let notiMsg = _$notification.querySelector('p').textContent;
+
+    _$notification.querySelector('p').textContent = `${notiMsg}, Numbah ${uiCount++}`;
 
     _$notification.querySelector('button.close').addEventListener('click', (_clickEvt) => {
       let toRemove;
 
       if (_clickEvt.target.parentNode !== _$notification) { return; }
-    
+
       _$notification.hidePopover();
       _$notification.style.setProperty('display', 'none');
       _$notification.classList.add('gc-me');
 
       toRemove = notifications.indexOf(_$notification);
 
-      if (toRemove != -1) {
-        sweeps.push(notifications[toRemove]);
-        notifications = notifications.toSpliced(toRemove, 1);
-        _$notification.remove();
-      }
+      if (toRemove != -1) { return; }
+
+      notifications = notifications.toSpliced(toRemove, 1);
+      _$notification.remove();
     });
 
     document.querySelector('#toaster').appendChild(_$notification);
@@ -38,12 +39,11 @@ const makePageApp = () => {
       _$byeNoti.hidePopover();
       _$byeNoti.style.setProperty('display', 'none');
       _$byeNoti.classList.add('gc-me');
-      sweeps.push(_$byeNoti);
       _$byeNoti.remove();
     }
-    
+
     notifications.push(_$notification);
-    
+
   };
 
   return Object.freeze({
